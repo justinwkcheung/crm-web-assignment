3581667517fddc64582e6c74c4bbb704d067dab1
Justin Cheung <justinwkcheung@github.com>
2017-01-25 16:33:34 -0500

    Create edit contact form and updated contact list

--- crm.rb
+++ crm.rb
@@ -28,5 +28,33 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to('/')
+  else
+    raise Sinatra::NotFound
+  end
 end
