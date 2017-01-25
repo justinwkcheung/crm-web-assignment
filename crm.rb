12a4cd354feed604c63d0e5c65032124aa756ecc
Justin Cheung <justinwkcheung@github.com>
2017-01-25 15:36:58 -0500

    Complete home page with updating contact list and count

--- crm.rb
+++ crm.rb
@@ -9,19 +9,19 @@ require_relative 'contact'
 # Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
-  @crm_app_name = "Justin's CRM"
+  # @crm_app_name = "Justin's CRM"
   erb :index
 end
 
-get '/contacts' do
-  erb :contacts
-end
+# get '/contacts' do
+#   erb :contacts
+# end
 
 get '/contacts/new' do
   erb :new_contact
 end
 
-post '/contacts' do
+post '/' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-  redirect to('/contacts')
+  redirect to('/')
 end
